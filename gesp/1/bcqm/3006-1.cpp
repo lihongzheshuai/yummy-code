db845f3683392a646b7a1abb19da11ebdff61283
OneCoder <wushikezuo@gmail.com>
2024-09-12 22:18:05 +0800

    3006-1

---
+++ gesp/1/bcqm/3006-1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout << "ABCDEFGHIJKLMNOPQR" << endl;
+	cout << "BABCDEFGHIJKLMNOPQ" << endl;
+	cout << "CBABCDEFGHIJKLMNOP" << endl;
+	cout << "DCBABCDEFGHIJKLMNO" << endl;
+	cout << "EDCBABCDEFGHIJKLMN" << endl;
+	cout << "FEDCBABCDEFGHIJKLM" << endl;
+	cout << "GFEDCBABCDEFGHIJKL" << endl;
+	cout << "HGFEDCBABCDEFGHIJK" << endl;
+	cout << "IHGFEDCBABCDEFGHIJ" << endl;
+	cout << "JIHGFEDCBABCDEFGHI" << endl;
+	return 0;
+}
