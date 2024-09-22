ffaba282a6c57f5904333f221a4487dc4191f7b1
OneCoder <wushikezuo@gmail.com>
2024-09-22 21:33:03 +0800

    new temp code

---
+++ gesp/1/bcqm/3015.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+int main(){
+	// nÍ·Å£³ÔÒ»Ìì 
+	int n = 1;
+	int m, s;
+
+	
+	for(c=1;(a + b)/ c!=0;c++){
+		if((a + b) / c == 0){
+		break;	
+		}
+		}
+		printf("%d", c);
+		return 0;
+}
---
+++ gesp/1/bcqm/3024.cpp
@@ -0,0 +1,8 @@
+#include<cstdio>
+using namespace std;
+int main(){
+	int a, b;
+	scanf("%d%d", &a ,&b);
+	printf("×ÜµÄ¿ÃÊý£º%d", a * b);
+	return 0;
+}
