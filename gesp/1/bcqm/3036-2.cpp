9fba1b32704dd6b8fce65840f5ed8d871089bb1a
lihongzhe <wushikezuo@gmail.com>
2024-10-06 22:30:32 +0800

    3036

--- gesp/1/bcqm/3036-2.cpp
+++ gesp/1/bcqm/3036-2.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a;
-	cin >> a;
-	//b十位 ,c个位。 
-    int b, c;
-    b = a / 10;
-    c = a % 10;
-    int d;
-    d = c * 10 + b; 
-	cout << d;
-	return 0;
+int main() {
+    int a;
+    cin >> a;
+    int b, c, d;
+    b = a / 100;
+    c = a % 100 / 10;
+    d = a % 100 % 10;
+    int e;
+    e = d * 100 + c * 10 + b;
+    cout << e;
+    return 0;
 }
--- gesp/1/bcqm/3036.cpp
+++ gesp/1/bcqm/3036.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a;
-	cin >> a;
-	//b十位 ,c个位。 
+int main() {
+    int a;
+    cin >> a;
+    // b十位 ,c个位。
+    int b, c;
+    b = a / 10;
+    c = a % 10;
     int d;
-	d = a % 10*10+a/10;
-
-	cout << d;
-	return 0;
+    d = c * 10 + b;
+    cout << d;
+    return 0;
 }
