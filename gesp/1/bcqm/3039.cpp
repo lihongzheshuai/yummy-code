028feeb08c87765f3bc8a938b93955700f4ad3fd
lihongzhe <wushikezuo@gmail.com>
2024-10-08 09:22:59 +0800

    3039

--- gesp/1/bcqm/3039.cpp
+++ gesp/1/bcqm/3039.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a, b;
-	cin >> a >> b;
-	cout << a / b << " " << a % b;
-	return 0;
-} 
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a / b << " " << a % b;
+    return 0;
+}
