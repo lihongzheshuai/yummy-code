8208519abe487e6f25707ff48a1f2e713c6d8fd4
lihongzhe <wushikezuo@gmail.com>
2024-10-10 13:23:57 +0800

    3046

--- GESP2.md
+++ GESP2.md
@@ -5,3 +5,4 @@
 | BCQM3042| ASCII码-从数字到字符| ★☆☆☆☆ | ✅ |[BCQM3042](https://www.coderli.com/gesp-1-bcqm3042)|
 | BCQM3044| ASCII码-大小写转换| ★☆☆☆☆ | ✅ |[BCQM3043](https://www.coderli.com/gesp-1-bcqm3043)|
 | BCQM3045| ASCII码-从字符到数字| ★☆☆☆☆ | ✅ |[BCQM3045](https://www.coderli.com/gesp-1-bcqm3045)|
+| BCQM3046| ASCII码-加四加密| ★☆☆☆☆ | ✅ |[BCQM3046](https://www.coderli.com/gesp-1-bcqm3046)|
--- gesp/1/bcqm/3046-2.cpp
+++ gesp/1/bcqm/3046-2.cpp
@@ -1,11 +1,11 @@
-#include<cstdio>
+#include <cstdio>
 using namespace std;
-int main(){
-	char str[5];
-	scanf("%s", str);
-	for(int i = 0; i < 5; i++) {
-		char c = str[i] + 4;
-		printf("%c", c);
-	}
-	return 0;
+int main() {
+    char str[5];
+    scanf("%s", str);
+    for (int i = 0; i < 5; i++) {
+        char c = str[i] + 4;
+        printf("%c", c);
+    }
+    return 0;
 }
