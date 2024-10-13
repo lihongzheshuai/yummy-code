da3dd32c1efa24f14d79483b6d5bcfc5f2f68fc3
OneCoder <wushikezuo@gmail.com>
2024-10-13 22:23:44 +0800

    code

---
+++ gesp/1/bcqm/3084.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cin >> a;
+	if(a > 140 && a <= 200){
+		cout << "Ìì²Å";
+	}else{
+		cout << " ";
+	}
+	return 0;
+} 
---
+++ gesp/1/bcqm/3145.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int m, n;
+	cin >> m >> n;
+	int i=0;
+	for(m;m <= n;m++){
+		if(m % 2 != 0){
+			i += m;
+		}
+	}
+	cout << i;
+	return 0;
+}
