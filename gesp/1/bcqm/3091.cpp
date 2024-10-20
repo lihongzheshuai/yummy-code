c2ad3198235ae1f76dd3a536585f3669cd58393d
OneCoder <wushikezuo@gmail.com>
2024-10-20 23:13:18 +0800

    3091

--- gesp/1/bcqm/3091.cpp
+++ gesp/1/bcqm/3091.cpp
@@ -1,34 +1,24 @@
-#include<cstdio>
+#include <cstdio>
 using namespace std;
-int main(){
-	char n[5];
-	scanf("%s", n);
-	int i = 0;
-	if('D' == n[0] ){
-		i += 30;
-	}else{
-		i += 0;
-	}
-	if('C' ==  n[1]){
-		i += 30;
-	}else{
-		i += 0;
-	}
-	if('B' == n[2] ){
-		i += 30;
-	}else{
-		i += 0;
-	}
-	if('A' == n[3]){
-		i += 30;
-	}else{
-		i += 0;
-	}
-	if( 'D' == n[4]  ){
-		i += 30;
-	}else{
-		i += 0;
-	}
-	printf("%d", i);
-	return 0;
-} 
+int main() {
+    char n[5];
+    scanf("%s", n);
+    int i = 0;
+    if ('D' == n[0]) {
+        i += 30;
+    }
+    if ('C' == n[1]) {
+        i += 30;
+    }
+    if ('B' == n[2]) {
+        i += 30;
+    }
+    if ('A' == n[3]) {
+        i += 30;
+    }
+    if ('D' == n[4]) {
+        i += 30;
+    }
+    printf("%d", i);
+    return 0;
+}
