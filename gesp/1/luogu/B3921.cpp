21327dc850398060e8b628987186afa1415e8be2
lihongzhe <wushikezuo@gmail.com>
2024-10-28 22:05:24 +0800

    b3921

--- gesp/1/luogu/B3921.cpp
+++ gesp/1/luogu/B3921.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main(){
 	int a, b;
 	cin >> a >> b;
-	int i = a + b;
-	if(i > 7){
-		i -= 7; 
+	int i = b % 7;
+	int j = a + i;
+	if(j > 7){
+		j -= 7;
 	}
-	cout << i;
+	cout << j;
 	return 0;
 } 
