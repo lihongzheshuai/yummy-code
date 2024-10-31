502859ed216c53e3e5839d7f5b69f7fb341e5636
OneCoder <wushikezuo@gmail.com>
2024-10-31 15:04:00 +0800

    b3922

--- gesp/1/luogu/B3922.cpp
+++ gesp/1/luogu/B3922.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a, b;
-	cin >> a >> b;
-	int i = 1;
-	for(i;i <= a;i++){
-		if(i % b != 0){
-			cout << i << endl;
-		}
-	}
-	return 0;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    for (int i = 1; i <= a; i++) {
+        if (i % b != 0) {
+            cout << i << endl;
+        }
+    }
+    return 0;
 }
