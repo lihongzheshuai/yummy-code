17bbf55a0ded2aabe65d87be2740491524806e4c
OneCoder <wushikezuo@gmail.com>
2024-11-07 23:25:59 +0800

    b4001

--- gesp/1/luogu/B4001.cpp
+++ gesp/1/luogu/B4001.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int n;
-	cin >> n;
-	int u = 0;
-	for(int i = 1;i <= n;i++){
-		if(i * i * i == n){
-			u += 1;
-		}else{
-			u += 0;
-		}
-	}
-	if(u == 0 ){
-		cout << "No";
-	}else{
-		cout << "Yes";
-	}
-	return 0;
+int main() {
+    int n;
+    cin >> n;
+    int u = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i * i * i == n) {
+            u += 1;
+        } else {
+            u += 0;
+        }
+    }
+    if (u == 0) {
+        cout << "No";
+    } else {
+        cout << "Yes";
+    }
+    return 0;
 }
