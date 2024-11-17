bc32b7fcdf8574d862b41d05b8797c1464edc38c
OneCoder <wushikezuo@gmail.com>
2024-11-17 22:47:42 +0800

    b2057

--- gesp/1/luogu/B2057.cpp
+++ gesp/1/luogu/B2057.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int n;
-	cin >> n;
-	int max = 0;
-	int point;
-	for(int i = 1;i <= n;i++){
-		cin >> point;
-		if(point > max){
-			max = point;
-		}
-		}
-	cout << max;
-	return 0;
+int main() {
+    int n;
+    cin >> n;
+    int max = 0;
+    int point;
+    for (int i = 1; i <= n; i++) {
+        cin >> point;
+        if (point > max) {
+            max = point;
+        }
+    }
+    cout << max;
+    return 0;
 }
