697e11270f2a6c11a9f6abdbaf089cdbe9372035
OneCoder <wushikezuo@gmail.com>
2024-11-18 23:34:09 +0800

    b2070

--- gesp/1/luogu/B2070.cpp
+++ gesp/1/luogu/B2070.cpp
@@ -1,17 +1,17 @@
-#include<cstdio>
+#include <cstdio>
 using namespace std;
-int main(){
-	int n;
-	scanf("%d", &n);
-	double ans = 0;
-	for(int i = 1;i<=n;i++){
-		if(i % 2 != 0){
-			ans += 1.0 / i;
-		}
-		if(i % 2 == 0){
-			ans -= 1.0 / i;
-		}
-	}
-	printf("%.4f", ans);
-	return 0;
+int main() {
+    int n;
+    scanf("%d", &n);
+    double ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) {
+            ans += 1.0 / i;
+        }
+        if (i % 2 == 0) {
+            ans -= 1.0 / i;
+        }
+    }
+    printf("%.4f", ans);
+    return 0;
 }
