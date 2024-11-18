53f8e04add65c5bf2b282538f3f4a198ea992f59
OneCoder <wushikezuo@gmail.com>
2024-11-18 15:57:08 +0800

    b3648

--- gesp/1/luogu/B3661.cpp
+++ gesp/1/luogu/B3661.cpp
@@ -1,22 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int n;
-	cin >> n;
-	int* input_array = new int[n]; 
-	for(int i = 0;i < n;i++){
-		cin >> input_array[i];
-	}
-	for(int i = 0; i < n; i++) {
-		if(input_array[i] % 2 != 0){
-			cout << input_array[i] << " ";
-		}
-	} 
-	cout << endl;
-	for(int i = 0;i < n;i++){
-		if(input_array[i] % 2 == 0){
-			cout << input_array[i] << " ";
-		}		
-	}
-	return 0;
-} 
+int main() {
+    int n; // 读取数组长度
+    cin >> n;
+    int* input_array = new int[n]; // 动态分配数组空间
+    for (int i = 0; i < n; i++) {
+        cin >> input_array[i]; // 读取数组元素
+    }
+    // 打印奇数
+    for (int i = 0; i < n; i++) {
+        if (input_array[i] % 2 != 0) {
+            cout << input_array[i] << " ";
+        }
+    }
+    cout << endl; // 换行，分隔奇数和偶数
+    // 打印偶数
+    for (int i = 0; i < n; i++) {
+        if (input_array[i] % 2 == 0) {
+            cout << input_array[i] << " ";
+        }
+    }
+    return 0;
+}
