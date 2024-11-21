a645339ec8e3079ab9a39f01e1791d565c4d632d
OneCoder <wushikezuo@gmail.com>
2024-11-21 09:05:47 +0800

    fix post b2052

--- GESP1.md
+++ GESP1.md
@@ -92,3 +92,4 @@
 | luogu-B2032| 基本运算-等差数列末项计算| ★☆☆☆☆ | ✅ |[luogu-B2032](https://www.coderli.com/gesp-1-luogu-b2032/)|
 | luogu-B2043| 取余-判断能否被 3，5，7 整除| ★☆☆☆☆ | ✅ |[luogu-B2043](https://www.coderli.com/gesp-1-luogu-b2043/)|
 | luogu-B2044| if-有一门课不及格的学生| ★☆☆☆☆ | ✅ |[luogu-B2044](https://www.coderli.com/gesp-1-luogu-b2044/)|
+| luogu-B2052| if-简单计算器| ★☆☆☆☆ | ✅ |[luogu-B2052](https://www.coderli.com/gesp-1-luogu-b2052/)|
--- gesp/1/luogu/B2052.cpp
+++ gesp/1/luogu/B2052.cpp
@@ -1,26 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a, b;
-	char c;
-	cin >> a >> b >> c;
-	if((b == 0 && c == '/')){
-		cout << "Divided by zero!";
-	}
-	if(c == '+' && b != 0){
-		cout << a + b;
-	}
-	if(c == '-' && b != 0){
-		cout << a - b;
-	}
-	if(c == '*' && b != 0){
-		cout << a * b;
-	}
-	if(c == '/' && b != 0){
-		cout << a / b;
-	}
-	if(((c != '+' && c != '-') && c != '*') && c != '/'){
-		cout << "Invalid operator!";
-	}
-	return 0;
+int main() {
+    int a, b;
+    char c;
+    cin >> a >> b >> c;
+    if ((b == 0 && c == '/')) {
+        cout << "Divided by zero!";
+    }
+    if (c == '+' && b != 0) {
+        cout << a + b;
+    }
+    if (c == '-' && b != 0) {
+        cout << a - b;
+    }
+    if (c == '*' && b != 0) {
+        cout << a * b;
+    }
+    if (c == '/' && b != 0) {
+        cout << a / b;
+    }
+    if (((c != '+' && c != '-') && c != '*') && c != '/') {
+        cout << "Invalid operator!";
+    }
+    return 0;
 }
