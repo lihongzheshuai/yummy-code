4ad31d7a035e849a2f8de369217bf4d344eac3a8
OneCoder <wushikezuo@gmail.com>
2024-12-02 15:19:41 +0800

    p1307

--- gesp/1/luogu/P1307.cpp
+++ gesp/1/luogu/P1307.cpp
@@ -1,30 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a;
-	cin >> a;
-	int* array = new int[10];
-	int index = 0;
-	int tmp=0;
-	if(a == 0){
-		cout << 0;
-		return 0;
-	}
-	while(a != 0){
-		int ans = a % 10;
-		array[index] = ans;
-		a /= 10;
-		if(a == 0){
-			break;
-		}else{
-			index++;
-		}
-	}
-	int y = 1;
-	for(int i = index; i >= 0; i--){
-	 	tmp += array[i] * y;
-	 	y *= 10;
-	}
-	cout << tmp;
-	return 0;
+int main() {
+    int a;
+    cin >> a;
+    int* array = new int[10];
+    int index = 0;
+    int tmp = 0;
+    if (a == 0) {
+        cout << 0;
+        return 0;
+    }
+    while (a != 0) {
+        int ans = a % 10;
+        array[index] = ans;
+        a /= 10;
+        if (a == 0) {
+            break;
+        } else {
+            index++;
+        }
+    }
+    int y = 1;
+    for (int i = index; i >= 0; i--) {
+        tmp += array[i] * y;
+        y *= 10;
+    }
+    cout << tmp;
+    return 0;
 }
