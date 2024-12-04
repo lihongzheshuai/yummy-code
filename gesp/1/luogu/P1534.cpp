731e9acd78cb4029a8df5dfe07b3f24329de05ae
OneCoder <wushikezuo@gmail.com>
2024-12-04 13:40:56 +0800

    p1534

--- gesp/1/luogu/P1534.cpp
+++ gesp/1/luogu/P1534.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a;
-	cin >> a;
-	int b, c;
-	int ans = 0;
-	int y = 0;
-	for(int i = 1;i <= a;i++){
-		cin >> b >> c;
-		int t = (c + b) - 8;
-		int t_1 = t + y;
-		ans += t_1;
-		y += t;
-	}
-	cout << ans;
-	return 0;
+int main() {
+    int a;
+    cin >> a;
+    int b, c;
+    int ans = 0;
+    int y = 0;
+    for (int i = 1; i <= a; i++) {
+        cin >> b >> c;
+        int t = (c + b) - 8;
+        int t_1 = t + y;
+        ans += t_1;
+        y += t;
+    }
+    cout << ans;
+    return 0;
 }
