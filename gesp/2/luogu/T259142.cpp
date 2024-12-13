980b9116726301a175d82d39fb565eca8f5d6a56
OneCoder <wushikezuo@gmail.com>
2024-12-13 20:32:06 +0800

    t259412

--- gesp/2/luogu/T259142.cpp
+++ gesp/2/luogu/T259142.cpp
@@ -1,17 +1,17 @@
-#include<cstdio>
-int main(){
-	int a, b, ans=0;
-	scanf("%d%d", &a,&b);
-	for(int i = 1;i<=a;i++){
-		int d = i;
-		while(d != 0){
-			ans = d % 10;
-			if(ans == b){
-				ans++;
-				d = d / 10; 
-			}
-		}
-	}
-	printf("%d", ans);
-	return 0;
+#include <cstdio>
+int main() {
+    int a, b, ans = 0;
+    scanf("%d%d", &a, &b);
+    for (int i = 1; i <= a; i++) {
+        int d = i;
+        while (d != 0) {
+            ans = d % 10;
+            if (ans == b) {
+                ans++;
+                d = d / 10;
+            }
+        }
+    }
+    printf("%d", ans);
+    return 0;
 }
