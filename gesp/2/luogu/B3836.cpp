e0a01cb685bafd0f642402578bf0d3f5c4eae047
lihongzhe <wushikezuo@gmail.com>
2025-01-14 22:25:09 +0800

    250114 yummy

---
+++ gesp/2/luogu/B3836.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	int ans = 0;
+	for(int i = 0;i <= e;i++){
+		for(int j = 0;j <= e;j++){
+
+				int chi = e - i - j;
+
+				if(chi >= 0 && chi % c == 0 && a * i + b * j + chi / c == d){
+					ans++;
+				}
+		}
+	}
+	cout << ans;
+	return 0;
+}
