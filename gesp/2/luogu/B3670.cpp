09234dc91affc34243918dcff5c7812ce22a1e2c
lihongzhe <wushikezuo@gmail.com>
2025-02-04 09:42:31 +0800

    yummy code

---
+++ gesp/2/luogu/B3670.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n, r;
+	cin >> n >> r;
+	int k, p;
+	int max = 0;
+	for(int i = 1;i<=n;i++){
+		cin >> k >> p;
+		if(k > max && p <=r){
+			max = k;
+		} 
+	}
+	cout << max;
+	return 0;
+} 
