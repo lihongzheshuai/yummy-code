62b2872c5b128881ad2fe26acdd244b623b19dca
lihongzhe <wushikezuo@gmail.com>
2025-02-05 21:14:14 +0800

    yummy code 2502025

---
+++ gesp/2/luogu/B3954.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cin >> a;
+	int b;
+	long long ans = 1;
+	for(int i = 1;i<=a;i++){
+		cin >> b;
+		ans *= b;
+	}
+	if(ans <= 1000000){
+		cout << ans;
+	}else{
+		cout << ">1000000";
+	}
+	return 0;
+} 
---
+++ gesp/2/luogu/texe/1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char a = '3';
+	char b = '6';
+	cout << (char)(a+b);
+	return 0;
+}
