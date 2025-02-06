02d68309fe903c9febb583d9ad48d84064991aab
OneCoder <wushikezuo@gmail.com>
2025-02-06 10:41:54 +0800

    b3954

--- GESP2.md
+++ GESP2.md
@@ -52,3 +52,4 @@
 | luogu-B3866| 真题-数字黑洞| ★✮☆☆☆ | ✅ |[luogu-B3866](https://www.coderli.com/gesp-2-luogu-b3866/)|
 | luogu-B3923| 真题-小杨做题| ★✮☆☆☆ | ✅ |[luogu-B3923](https://www.coderli.com/gesp-2-luogu-b3923/)|
 | luogu-B3924| 真题-小杨的H字矩阵| ★✮☆☆☆ | ✅ |[luogu-B3924](https://www.coderli.com/gesp-2-luogu-b3924/)|
+| luogu-B3954| 真题-乘法问题| ★✮☆☆☆ | ✅ |[luogu-B3954](https://www.coderli.com/gesp-2-luogu-b3954/)|
--- gesp/2/luogu/B3954.cpp
+++ gesp/2/luogu/B3954.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a;
-	cin >> a;
-	int b;
-	long long ans = 1;
-	for(int i = 1;i<=a;i++){
-		cin >> b;
-		ans *= b;
-	}
-	if(ans <= 1000000){
-		cout << ans;
-	}else{
-		cout << ">1000000";
-	}
-	return 0;
-} 
+int main() {
+    int a;
+    cin >> a;
+    int b;
+    long long ans = 1;
+    for (int i = 1; i <= a; i++) {
+        cin >> b;
+        ans *= b;
+    }
+    if (ans <= 1000000) {
+        cout << ans;
+    } else {
+        cout << ">1000000";
+    }
+    return 0;
+}
