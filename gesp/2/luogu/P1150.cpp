13f55980eaa9284a10ef3f443b87e9f9fcfb12b9
OneCoder <wushikezuo@gmail.com>
2025-02-07 21:26:28 +0800

    p1150 b3994

--- gesp/2/luogu/P1150.cpp
+++ gesp/2/luogu/P1150.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	long long n, k;
-	cin >> n >> k;
-	long long ans = 0;
-	for(long long i = 1;i<=n;i++){
-		if(i%k==0){
-			n++;
-		}
-		ans+=1;
-	}
-	cout << ans;
-	return 0;
+int main() {
+    long long n, k;
+    cin >> n >> k;
+    long long ans = 0;
+    for (long long i = 1; i <= n; i++) {
+        if (i % k == 0) {
+            n++;
+        }
+        ans += 1;
+    }
+    cout << ans;
+    return 0;
 }
