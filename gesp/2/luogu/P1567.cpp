7600595e864041025d228787010ebe91df674223
lihongzhe <wushikezuo@gmail.com>
2025-02-08 10:49:26 +0800

    yummy code 250208

---
+++ gesp/2/luogu/P1567.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int a[1000001];
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	int total_max = 1;
+	if (n == 1) {
+		cout << total_max;
+		return 0;
+	}
+	int last_num = a[0];
+	int cur_max = 1;
+	for (int i = 1; i < n; i++) {
+		if (a[i] > last_num) {
+			cur_max++;
+		} else {
+			total_max = max(total_max, cur_max);
+			cur_max = 1;
+		}
+		last_num = a[i];
+	}
+	total_max = max(total_max, cur_max);
+	cout << total_max;
+	return 0;
+
+}
