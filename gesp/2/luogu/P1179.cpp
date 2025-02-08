1232b19eb5b15311682525e06c8559053e5ce5e4
OneCoder <wushikezuo@gmail.com>
2025-02-08 12:28:30 +0800

    p1179

--- gesp/2/luogu/P1179.cpp
+++ gesp/2/luogu/P1179.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int l, r;
-	cin >> l >> r;
-	int ans = 0;
-	for(int i = l;i<=r;i++){
-		int j, re=i;
-		while(re!=0){
-			j=re%10;
-			if(j==2){
-				ans++;
-			}
-			re/=10;
-		}
-	}
-	cout << ans;
-	return 0;
-} 
+int main() {
+    int l, r;
+    cin >> l >> r;
+    int ans = 0;
+    for (int i = l; i <= r; i++) {
+        int j, re = i;
+        while (re != 0) {
+            j = re % 10;
+            if (j == 2) {
+                ans++;
+            }
+            re /= 10;
+        }
+    }
+    cout << ans;
+    return 0;
+}
