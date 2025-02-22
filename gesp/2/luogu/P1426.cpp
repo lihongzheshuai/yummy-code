e1b111d27c0454c642c5ed14a2cb9f31549c69d9
lihongzhe <wushikezuo@gmail.com>
2025-02-22 12:36:28 +0800

    yummy code 250222

---
+++ gesp/2/luogu/P1426.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	double s, x;
+	cin >> s >> x;
+	double t = 0;
+	double ans = 0;
+	for(int i = 1;;i++){
+			if(ans >=s-x){
+			if(ans+t*0.98>s+x){
+				cout << "n";
+				return 0;
+			}else{
+				cout << "y";
+				return 0;
+			}
+		}
+		if(i==1){
+			t+=7;
+		}else{
+			t *= 0.98;
+		}
+		ans+=t;
+	
+	}
+	return 0;
+}
