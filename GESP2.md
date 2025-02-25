7ebb937fbfa0e8a0cc361052f8d23b9a103e804e
OneCoder <wushikezuo@gmail.com>
2025-02-25 10:36:52 +0800

    b3707

--- GESP2.md
+++ GESP2.md
@@ -69,3 +69,5 @@
 | luogu-B3710| 多层循环-破碎的历史| ★✮☆☆☆ | ✅ |[luogu-B3710](https://www.coderli.com/gesp-2-luogu-b3710/)|
 | luogu-B3698| 多层循环-一次函数| ★✮☆☆☆ | ✅ |[luogu-B3698](https://www.coderli.com/gesp-2-luogu-b3698/)|
 | luogu-B2075| 多层循环-幂的末尾| ★✮☆☆☆ | ✅ |[luogu-B2075](https://www.coderli.com/gesp-2-luogu-b2075/)|
+| luogu-B3707| 多层循环-风神瞳| ★✮☆☆☆ | ✅ |[luogu-B3707](https://www.coderli.com/gesp-2-luogu-b3707/)|
+
--- gesp/2/luogu/B3707.cpp
+++ gesp/2/luogu/B3707.cpp
@@ -1,28 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int p,x,y,g,h;
-	cin >> p >> x >> y >> g >> h;
-	if(p == 1){
-		if(x == g || y == h){
-			cout << "Yes";
-		}else{
-			cout << "No";
-		}
-	}else if(p == 2){
-		cout << "No";
-	}else if(p==3){
-		if((g == x+2&&h==y+1)||(g==x+2&&h ==y-2)||(g==x-2&&h==y+1)||(g == x - 2&&h == y - 1)|| (g == x + 1 && h ==  y + 2 ) || ( g ==  x + 1 && h == y - 2 ) || (g == x - 1 && h == y +  2 ) || (g == x - 1 && h == y - 2)){
-			cout << "Yes";
-		}else{
-			cout << "No";
-		}
-	}else{
-		if((g ==x+2&&h==y+2)||(g == x+2&&h == y-2)||(g ==x-2&&h==y+2)||(g==x-2&&h == y - 2)){
-			cout << "Yes";
-		}else{
-			cout << "No";
-		}
-	}
-	return 0;
+int main() {
+    int p, x, y, g, h;
+    cin >> p >> x >> y >> g >> h;
+    if (p == 1) {
+        if (x == g || y == h) {
+            cout << "Yes";
+        } else {
+            cout << "No";
+        }
+    } else if (p == 2) {
+        cout << "No";
+    } else if (p == 3) {
+        if ((g == x + 2 && h == y + 1) || (g == x + 2 && h == y - 2) ||
+            (g == x - 2 && h == y + 1) || (g == x - 2 && h == y - 1) ||
+            (g == x + 1 && h == y + 2) || (g == x + 1 && h == y - 2) ||
+            (g == x - 1 && h == y + 2) || (g == x - 1 && h == y - 2)) {
+            cout << "Yes";
+        } else {
+            cout << "No";
+        }
+    } else {
+        if ((g == x + 2 && h == y + 2) || (g == x + 2 && h == y - 2) ||
+            (g == x - 2 && h == y + 2) || (g == x - 2 && h == y - 2)) {
+            cout << "Yes";
+        } else {
+            cout << "No";
+        }
+    }
+    return 0;
 }
