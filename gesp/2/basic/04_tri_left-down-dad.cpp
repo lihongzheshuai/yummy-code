910df123693973563b93676234c391fe7440ac6b
OneCoder <wushikezuo@gmail.com>
2025-03-20 20:05:21 +0800

    04_tri_left_down

--- gesp/2/basic/04_tri_left-down-dad.cpp
+++ gesp/2/basic/04_tri_left-down-dad.cpp
@@ -5,7 +5,7 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (j <= i) {
+            if (j < n - i) {
                 cout << "*";
             } else {
                 cout << " ";
