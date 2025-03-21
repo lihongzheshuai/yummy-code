252e44cb708a5ea7be73cdf0db01aaefda702f52
lihongzhe <wushikezuo@gmail.com>
2025-03-21 21:50:33 +0800

    yummy code 250321

---
+++ gesp/2/luogu/picture09.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	int ans = (n+1)/2;
+	for(int i = 1;i<=n;i++){
+		for(int j = 1;j<=n;j++){
+			if(i==ans&&j==ans){
+				cout << "*";
+			}else if(i<ans){
+				if(j==ans-i+1||j==ans+i-1){
+					cout <<"*";
+				}else{
+					cout <<" "; 
+				}
+			}else{
+				if(j==i-ans+1||j==n-i+ans){
+					cout << "*";
+				}else{
+					cout <<" "; 
+				}
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
